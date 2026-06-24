f92a509fbe6804558427e080e2117f4f732f8909
giulianaunrein <giuliana.unrein@gmail.com>
2026-06-24 17:45:57 -0300

    Update vuelos.js

--- js/vuelos.js
+++ js/vuelos.js
@@ -7,8 +7,19 @@ const contenedorNacional = document.getElementById("vuelos-nacionales");
 const contenedorInternacional = document.getElementById("vuelos-inter");
 
 
-
-
+function guardarBusquedaYNavegar(vuelo) {
+    const busqueda = {
+        origen:      vuelo.origen,
+        destino:     vuelo.destino,
+        fechaIda:    "",
+        fechaVuelta: "",
+        pasajeros:   "1",
+        clase:       "Económica",
+        tipoVuelo:   "Ida y vuelta"
+    };
+    sessionStorage.setItem("busquedaVuelo", JSON.stringify(busqueda));
+    window.location.href = "../pages/resultados.html";
+}
 
 function mostrarVuelos(lista, contenedorId) {
 
@@ -16,41 +27,38 @@ function mostrarVuelos(lista, contenedorId) {
 
     lista.forEach(vuelo => {
 
-        contenedor.innerHTML += `
-            <div class="tarjeta">
-
-                <img src="${vuelo.imagen}" alt="${vuelo.destino}">
-
-                <div class="info">
-
-                    <div class="parte-arriba">
-                        <h3>${vuelo.destino}</h3>
-                        <p>${vuelo.pais}</p>
-                    </div>
-
-                    <div class="parte-abajo">
-                        <p>Desde</p>
-                        <p class="precio">US$${vuelo.precioBase}</p>
-
-                        <div class="ver-vuelos">
-                            <a href="../pages/resultados.html">
-                                <p>Ver vuelos</p>
-                            </a>
-
-                            <a href="../pages/resultados.html">
-                                <img src="../images/flecha-naranja.png" alt="flecha">
-                            </a>
-                        </div>
-
-                    </div>
-
+        const tarjeta = document.createElement("div");
+        tarjeta.className = "tarjeta";
+        tarjeta.innerHTML = `
+            <img src="${vuelo.imagen}" alt="${vuelo.destino}">
+            <div class="info">
+                <div class="parte-arriba">
+                    <h3>${vuelo.destino}</h3>
+                    <p>${vuelo.pais}</p>
                 </div>
-
+                <div class="parte-abajo">
+                    <p>Desde</p>
+                    <p class="precio">US$${vuelo.precioBase}</p>
+                    <div class="ver-vuelos">
+                        <a class="btn-ver-vuelos">
+                            <p>Ver vuelos</p>
+                        </a>
+                        <a class="btn-ver-vuelos">
+                            <img src="../images/flecha-naranja.png" alt="flecha">
+                        </a>
+                    </div>
+                </div>
             </div>
         `;
+
+        tarjeta.querySelectorAll(".btn-ver-vuelos").forEach(btn => {
+            btn.style.cursor = "pointer";
+            btn.addEventListener("click", () => guardarBusquedaYNavegar(vuelo));
+        });
+
+        contenedor.appendChild(tarjeta);
     });
 }
-
 
 
 function carrusel(contenedor, btnAnterior, btnSiguiente){
@@ -69,6 +77,4 @@ mostrarVuelos(vuelosNacionales, "vuelos-nacionales");
 mostrarVuelos(vuelosEuropa, "vuelos-inter");
 
 carrusel(contenedorNacional,document.getElementById("anterior-nacional"),document.getElementById("siguiente-nacional"));
-carrusel(contenedorInternacional,document.getElementById("anterior-inter"),document.getElementById("siguiente-inter")
-);
-
+carrusel(contenedorInternacional,document.getElementById("anterior-inter"),document.getElementById("siguiente-inter"));
